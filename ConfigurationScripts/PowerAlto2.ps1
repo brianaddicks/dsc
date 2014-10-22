1d7d229dadb88b2f3454871bb78e26cc2454ae7f
brianaddicks <brian@addicks.us>
2014-10-21 21:37:57 -0400

    who knows

---
+++ ConfigurationScripts/PowerAlto2.ps1
@@ -0,0 +1,18 @@
+﻿Configuration PowerAlto2 {
+    param (
+        [string[]]$ComputerName = 'localhost'
+    )
+
+    Import-DscResource -Name MSFT_xRemoteFile -ModuleName xPSDesiredStateConfiguration
+
+    Node $ComputerName {
+        xRemoteFile DownloadZip {
+            DestinationPath = "$env:temp\PowerAlto2.psd1"
+            Uri             = 'https://github.com/brianaddicks/poweralto2/raw/master/PowerAlto2.psd1'
+        }
+        Log InterestingInfo {
+            Message = "Whatever you'd like to say"
+            #DependsOn = "[File]MassiveFileCopy"
+        }
+    }
+}
